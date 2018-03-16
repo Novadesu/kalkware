c6ac84a0f0dc442a1b6f72fed06fdaf6e9aaf165
Kalkonen <37437382+Kalkonen@users.noreply.github.com>
2018-03-16 10:05:53 +0100

    main commit(first)

---
+++ KalkonenWare/Tools/Hacks/FarESP.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "../../stdafx.h"
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+int lastUser = 1;
+long lastUpdate = -1;
+
+//Ill post a dedicated thread later today
